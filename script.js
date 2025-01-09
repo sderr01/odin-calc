089b2c1fca6173951e8bfeee2590a7bbf370adea
sderr01 <sarah1ujp@gmail.com>
2025-01-09 16:04:43 -0500

    got the bare bones of the operator functions, pseudo code wise

---
+++ script.js
@@ -0,0 +1,26 @@
+// global variables
+let x;
+let y;
+let operator;
+
+function add(){
+    // return x + y;
+    
+}
+function sub(){
+    // return x - y;
+
+}
+function mult(){
+    // return x * y
+
+}
+function div(){
+    // return x / y, check if either is 0
+
+}
+
+x = prompt("Enter x");
+y = prompt("enter y");
+operator = prompt("enter operator");
+alert(x + y + operator);
