e12f5d6f569146cc95797b0eb81d16f87a437980
sderr01 <sarah1ujp@gmail.com>
2025-01-09 18:32:37 -0500

    when buttons are clicked, can differentiate between number and operator. also have logic for updating display in comments

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
-// global variables
-let x;
-let y;
-let operator;
+let display = document.getElementById("output");
+let displayContent = "";
+let calculator = document.getElementById("buttons");
+let runningToal, x, y, operator;
 
-function add(){
-    // return x + y;
+
+
+// operator functions
+
+function add(x,y){
+    return x + y;
+}
+function sub(x,y){
+    return x - y;
+
+}
+function mult(x,y){
+    return x * y;
+
+}
+function div(x,y){
+    return x / y;
+
+}
+
+
+// choosing the correct operator
+
+function operate(x, y, operator){
+    if (operator == "+"){
+        add(x,y);
+    }
+    else if (operator == "-"){
+        sub(x,y);
+    }
+    else if (operator == "*"){
+        mult(x,y);
+    }
+    else if (operator == "/"){
+        div(x,y);
+    }
+}
+
+
+// testing for operators, returns true if num, false if operator
+// based on classes est in html file
+function isNum(classlist){
+    return classlist.contains("number");
+}
+
+// thank u google, looked up js check classlist, returns t/f
+
+calculator.addEventListener("click", function(num){
+    let val = num.target.value;
+    let classlist = num.target.classList;
+    // console.log(classlist.contains("number"));
+    console.log(isNum(classlist));
+
     
-}
-function sub(){
-    // return x - y;
+    // console.log(e.target.value);
+    // displayContent += e.target.value;
+    // display.textContent = displayContent;
 
-}
-function mult(){
-    // return x * y
+})
 
-}
-function div(){
-    // return x / y, check if either is 0
-
-}
-
-x = prompt("Enter x");
-y = prompt("enter y");
-operator = prompt("enter operator");
-alert(x + y + operator);
